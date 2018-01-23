8673e094b11e6655bbb627729bde660a887f9da3
Unknown <martuska1196@gmail.com>
2018-01-23 09:15:55 +0100

    actividades UD3
    actividades de la unidad 3

---
+++ UD3N/src/ud3n/UD3N.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ud3n;
+
+/**
+ *
+ * @author marta
+ */
+public class UD3N {
+
+    /**
+     * @param numerador entero
+     * @param denominador entero
+     * @return
+     */
+    
+    public static int division (int numerador, int denominador){
+        return (numerador/denominador);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("resultado de la operación: "+division(2,3));
+                
+    }
+}
